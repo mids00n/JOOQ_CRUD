5a1b13dc326f09aee51ad776978ca9b249bb8076
mibar <mibaranets@gmail.com>
2024-01-15 18:58:36 +0200

    initial commit

---
+++ src/main/java/com/example/dada/DadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dada/DadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
